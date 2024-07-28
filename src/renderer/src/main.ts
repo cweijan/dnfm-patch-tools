f09ed5c7bf8e7d3c87a084e8f922b1af0d7e7ee4
cweijan <cweijan@163.com>
2024-07-28 14:18:56 +0800

    引入antd vue

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,6 +1,7 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import Antd from 'ant-design-vue';
 import App from './App.vue'
+import './assets/main.css'
+import 'ant-design-vue/dist/reset.css';
 
-createApp(App).mount('#app')
+createApp(App).use(Antd).mount('#app')
