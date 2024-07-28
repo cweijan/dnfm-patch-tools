d8af21f09634e5ed38e433c88b1f136863577a45
cweijan <cweijan@163.com>
2024-07-28 15:56:24 +0800

    增加参考资料跳转

--- src/main/index.ts
+++ src/main/index.ts
@@ -4,7 +4,6 @@ import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
 
 function createWindow(): void {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 900,
     height: 670,
@@ -35,40 +34,22 @@ function createWindow(): void {
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
-
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+  electronApp.setAppUserModelId('cweijan.dnfm.path.tools')
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
   })
-
-  // IPC test
-  ipcMain.on('ping', () => console.log('pong'))
-
+  ipcMain.on('open', (_event, link) => {
+    shell.openExternal(link)
+  })
   createWindow()
-
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
-
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -3,19 +3,28 @@ import type { ItemType, MenuProps } from 'ant-design-vue';
 import { reactive, ref } from 'vue';
 import SkipCheck from './pages/SkipCheck.vue';
 
-// const ipcHandle = () => window.electron.ipcRenderer.send('ping')
-const selectedKeys = ref<string[]>(['skipCheck']);
-const openKeys = ref<string[]>(['skipCheck']);
+const activeMenu = ref('跳检测')
+const selectedKeys = ref<string[]>(['跳检测']);
 
-function getItem(label: string, icon?: any, children?: ItemType[], type?: 'group',): ItemType {
-  return { key: label, icon, children, label, type, } as ItemType;
+interface Options {
+  group?: boolean;
+}
+
+function getItem(label: string, icon?: any, children?: ItemType[], option: Options = {}): ItemType {
+  const { group } = option;
+  const type = group ? 'group' : null;
+  return { key: label, icon, children, label, type } as ItemType;
 }
 const items: ItemType[] = reactive([
-  getItem('工具', null, [getItem('跳检测'), getItem('资源屏蔽')], 'group'),
+  getItem('工具', null, [getItem('跳检测'), getItem('资源屏蔽')], { group: true }),
+  getItem('资料', null, [getItem('NPK用途对照')], { group: true }),
 ]);
 
 const handleClick: MenuProps['onClick'] = e => {
-  console.log('click', e);
+  if (e.key == 'NPK用途对照') {
+    return window.electron.ipcRenderer.send('open', 'https://bbs.aladedalu.com/thread-2018-1-1.html')
+  }
+  activeMenu.value = e.key as string
 };
 </script>
 
@@ -23,12 +32,13 @@ const handleClick: MenuProps['onClick'] = e => {
   <a-space direction="vertical" :style="{ width: '100%' }" :size="[0, 48]">
     <a-layout>
       <a-layout-sider style="background-color: white;">
-        <a-menu v-model:openKeys="openKeys" v-model:selectedKeys="selectedKeys" style="width: 180px" mode="inline"
-          :items="items" @click="handleClick" />
+        <a-menu v-model:selectedKeys="selectedKeys" style="width: 180px" mode="inline" :items="items"
+          @click="handleClick" />
       </a-layout-sider>
       <a-layout>
         <a-layout-content style="background-color: white;">
-          <SkipCheck />
+          <SkipCheck v-show="activeMenu == '跳检测'" />
+          <!-- <Reference v-show="activeMenu == 'NPK用途对照'" /> -->
         </a-layout-content>
       </a-layout>
     </a-layout>
