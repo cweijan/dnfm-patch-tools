6b03db148c2a43c05f439eb1ee100b7a5b35f2b7
cweijan <cweijan@163.com>
2024-07-28 14:16:48 +0800

    更新Eslint规则

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -5,15 +5,19 @@ const ipcHandle = () => window.electron.ipcRenderer.send('ping')
 </script>
 
 <template>
-  <img alt="logo" class="logo" src="./assets/electron.svg" />
-  <div class="creator">Powered by electron-vite</div>
+  <img alt="logo" class="logo" src="./assets/electron.svg">
+  <div class="creator">
+    Powered by electron-vite
+  </div>
   <div class="text">
     Build an Electron app with
     <span class="vue">Vue</span>
     and
     <span class="ts">TypeScript</span>
   </div>
-  <p class="tip">Please try pressing <code>F12</code> to open the devTool</p>
+  <p class="tip">
+    Please try pressing <code>F12</code> to open the devTool
+  </p>
   <div class="actions">
     <div class="action">
       <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">Documentation</a>
